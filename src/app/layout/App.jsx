8a4d34b7225c38d7e856d27a2c54a2336ca53e76
Yogesh Rajmane <yogesh.rajmane91@gmail.com>
2020-03-19 21:06:04 +0530

    Commit H2

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,3 +1,8 @@
+//////////////////////////////////////////////
+//YouTube Video --How to setup git in project
+//https://www.youtube.com/watch?v=2oihkInZ880
+///////////////////////////////////////////////
+
 import React,{Component} from 'react';
 
 class App extends Component 
@@ -7,6 +12,7 @@ class App extends Component
     return (
       <div >
         <p>Yogesh Rajmane</p>
+        <h2>Yogs</h2>
       </div>
     );
   }
